8e1c1b9f0d67bf1cd78af2274be713d1be4c1051
NIDADAVOLU RAVITEJA <52151482+Raviteja3399@users.noreply.github.com>
2024-04-01 10:36:34 +0530

    Create Ec2.tf

---
+++ Ec2.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+resource "aws_instance" "ec2" {
+    ami="ami-080e1f13689e07408"
+    instance_type ="t2.micro"
+    tags = {
+        name="myec2"
+    }
+}
